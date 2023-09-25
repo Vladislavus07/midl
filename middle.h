82be75c0a6255d95b7b232980683c3d860e6f03d
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-25 21:50:22 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -10,10 +10,10 @@ int itc_len_num(long long number);
 int itc_sum_num(long long number);
 long long itc_multi_num(long long number);
 
-itc_max_num(long long number);
-itc_min_num(long long number);
-itc_rev_num(long long number);
-itc_null_count(long long number);
+int itc_max_num(long long number);
+int itc_min_num(long long number);
+int itc_rev_num(long long number);
+int itc_null_count(long long number);
 
 
 bool itc_mirror_num(long long number);
