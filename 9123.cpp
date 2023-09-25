0f7a87e7ff9e150653223df46e33877cc98bfbbc
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-25 21:38:39 +0300

    Add files via upload

---
+++ 9123.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+bool itc_mirror_num(long long number)
+    {
+        long long a=0;
+        long long b = number;
+        while (number!=0)
+        {
+            long long z=number % 10;
+            a=z+a*10;
+            number/=10;
+        }
+        return a==b;
+    }
+int itc_mirror_count(long long number)
+{
+    int temp = 0;
+    for (long long i = 1; i <= number; i++) {
+        if (itc_mirror_num(i)) {
+            temp++;
+        }
+    }
+    return temp;
+}
---
+++ middle.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef MIDDLE_H_INCLUDED
+#define MIDDLE_H_INCLUDED
+
+void itc_num_print(int number);
+int itc_len_num(long long number);
+int itc_sum_num(long long number);
+long long itc_multi_num(long long number);
+
+itc_max_num(long long number);
+itc_min_num(long long number);
+itc_rev_num(long long number);
+itc_null_count(long long number);
+
+
+bool itc_mirror_num(long long number);
+int itc_mirror_count(long long number);
+
+
+#endif // MIDDLE_H_INCLUDED
