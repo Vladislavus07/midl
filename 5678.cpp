97cdf6499a1fc921ef07531b409d4078a9400c29
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-25 22:05:27 +0300

    Update 5678.cpp

--- 5678.cpp
+++ 5678.cpp
@@ -13,13 +13,29 @@ int itc_max_num(long long number)
         }
         number=number/10;
     }
+    while (number<0)
+    {
+        if(number%10>a)
+        {
+            a=number%10;
+        }
+        number=number/10;
+    }
     return a;
 }
 
 int itc_min_num(long long number)
 {
     int a=number%10;
-    while (number!=0)
+    while (number>0)
+    {
+        if(number%10<a)
+        {
+            a=number%10;
+        }
+        number=number/10;
+    }
+    while (number<0)
     {
         if(number%10<a)
         {
@@ -41,7 +57,7 @@ int itc_rev_num(long long number)
     }
     while (a<0){
         a=a/10;
-        i++;
+        i--;
     }
     while (number==0)
     {
@@ -55,7 +71,19 @@ int itc_rev_num(long long number)
 int itc_null_count(long long number)
 {
     int a=0;
-    while (number!=0)
+    while (number==0)
+    {
+        return 1;
+    }
+    while (number>0)
+    {
+        if(number%10==0)
+        {
+            a++;
+        }
+        number=number/10;
+    }
+    while (number<0)
     {
         if(number%10==0)
         {
