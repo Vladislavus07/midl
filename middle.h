159eaaf1e2fc82f3e55987c5c2c4ce5594338f95
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-09-27 21:59:19 +0300

    Update middle.h

--- middle.h
+++ middle.h
@@ -18,6 +18,8 @@ int itc_null_count(long long number);
 
 bool itc_mirror_num(long long number);
 int itc_mirror_count(long long number);
-
+int itc_second_max_num(long long number);
+int itc_second_simple_max_num(long long number);
+long long itc_bin_num(long long number);
 
 #endif // MIDDLE_H_INCLUDED
