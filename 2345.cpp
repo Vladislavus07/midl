3870b366d73b6bdf9fdf5d0529fe99d225be4fbd
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-10-01 16:54:08 +0300

    Add files via upload

---
+++ 2345.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+long long itc_oct_num(long long number)
+{
+    long long a = 1;
+    long long b = 0;
+
+    while (number != 0) {
+        int z = number % 8;
+        b += z * a;
+        a *= 10;
+        number /= 8;
+    }
+
+    return b;
+}
+int itc_rev_bin_num(long long number)
+{
+    int a = 1;
+    int b = 0;
+
+    while (number != 0) {
+        int z = number % 10;
+        b += z * a;
+        a *= 2;
+        number /= 10;
+    }
+
+    return b;
+}
