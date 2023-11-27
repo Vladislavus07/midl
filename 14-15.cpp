75b30d144cb5f721d32eece7643c54b923f5c87c
Vladislavus07 <143996724+Vladislavus07@users.noreply.github.com>
2023-11-27 13:05:23 +0300

    Update 14-15.cpp

--- 14-15.cpp
+++ 14-15.cpp
@@ -31,14 +31,38 @@ int itc_rev_bin_num(long long number)
     return b;
 }
 
-string perevod(int number)
+#include <iostream>
+#include <string>
+using namespace std;
+
+string perevod(int n)
     {
     string st = "";
-    while (number > 0) {
-        int zifra = number % 3;
-        number = number / 3;
+    while (n > 0) {
+        int zifra = n % 3;
+        n = n / 3;
         char z = zifra + '0';
         st = z + st;
     }
     return st;
+}
+
+int main()
+{
+    int n;
+    string tr, tro;
+    cin >> n;
+    tr = perevod(n);
+    if (n % 3 == 0)
+    {
+        tr = "1" + tr + "02";
+    }
+    else
+    {
+        int ost = (n % 3) * 4;
+        tro = perevod(ost);
+        tr = tr + tro;
+    }
+    cout << tr;
+    return 0;
 }
